6196b15c81532ed973d9a24fdc774aed8a112e32
flangoria <audrey.mabry90@gmail.com>
2024-07-10 10:06:31 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
  <h1>Weather App</h1>
- <p>This project was coded by Audrey Mabry and is open-sourced on 
+ <footer>This project was coded by Audrey Mabry and is open-sourced on 
  <a href='https://github.com/aamabry/aamabry-react-weather' target='blank'> Github</a> and is hosted on <a href='https://aamabry-react-weather.netlify.app/'>Netlify</a>
- </p>
+ </footer>
+ </div>
     </div>
   );
 }
