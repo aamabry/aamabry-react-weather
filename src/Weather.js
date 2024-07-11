ad554544678768ed00952ab581cc9e6feb4ab97b
flangoria <audrey.mabry90@gmail.com>
2024-07-11 12:10:15 -0400

    Worked on the CSS

--- src/Weather.js
+++ src/Weather.js
@@ -10,7 +10,7 @@ export default function Weather(){
                 <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/>
                 </div>
                 <div className="col-3">
-                <input type="submit" value="search" className="btn btn-primary w-100"/>
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
                 </div>
                 </div>
             </form>
@@ -22,8 +22,13 @@ export default function Weather(){
 </ul>
 <div className="row">
     <div className="col-6">
-        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"/>
-        6°C
+        <div className="clearFix">
+        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" className="float-left"/>
+        <div className="float-left">
+        <span className="temperature">6</span>
+        <span className="unit">°C</span>
+        </div>
+        </div>
     </div>
     <div className="col-6">
         <ul>
