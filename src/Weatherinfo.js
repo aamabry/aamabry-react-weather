49146c520ced555ac8e8841a694086a1c3e70e99
flangoria <audrey.mabry90@gmail.com>
2024-07-13 18:37:28 -0400

    Injected animated weather icons

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props){
     return(
@@ -14,8 +15,9 @@ export default function WeatherInfo(props){
 <div className="row mt-3">
     <div className="col-6">
         <div className="clearFix">
-        <img src= {props.data.iconUrl} alt={props.data.description} />
-       
+            <div className="float-left">
+            <WeatherIcon code={props.data.icon} />
+            </div>
         <span className="temperature" >{Math.round(props.data.temperature)}</span>
         <span className="unit">°C</span>
         </div>
