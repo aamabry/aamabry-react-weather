79371c55288afcfb5654cbc60302a52367c4335c
flangoria <audrey.mabry90@gmail.com>
2024-07-20 10:50:52 -0400

    Fixed broken codes

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -4,59 +4,17 @@ import WeatherIconSmall from "./WeatherIconSmall";
 
 export default function WeatherForecastDay(props){
     function maxTemperature(){
-        let temperature = Math.round(props.data.temp.max);
+        let temperature = Math.round(props.data.temperature.maximum);
         return `${temperature}°`;
     }
 
     function minTemperature() {
-        let temperature = Math.round(props.data.temp.min);
-        return `${temperature}`;
-    }
-
-    function day(){
-        let date = new Date();
-        let day = date.getDay();
-
-        let days = [
-            "Sun",
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thur",
-            "Fri",
-            "Sat"
-        ];
-
-        return days[day];
-    }
-
-    return (
-        <div>
-        <div className="WeatherForecast-day">{props.data.dt}</div>
-        <WeatherIconSmall />
-        <div className="WeatherForescast-temp">
-            <span className="WeatherForecast-temp-max">{maxTemperature}°</span>
-            <span className="WeatherForecast-temp-min">{minTemperature}17°</span>
-        </div>
-    </div>
-    );
-    }import React from "react";
-import WeatherIconSmall from "./WeatherIconSmall";
-
-
-export default function WeatherForecastDay(props){
-    function maxTemperature(){
-        let temperature = Math.round(props.data.temp.maximum);
-        return `${temperature}°`;
-    }
-
-    function minTemperature() {
-        let temperature = Math.round(props.data.temp.minimum);
+        let temperature = Math.round(props.data.temperature.minimum);
         return `${temperature}°`;
     }
 
     function day() {
-        let date = new Date(props.data.dt * 1000);
+        let date = new Date(props.data.temperature.day * 1000);
         let day = date.getDay();
 
         let days = [
@@ -75,7 +33,7 @@ export default function WeatherForecastDay(props){
     return (
         <div>
         <div className="WeatherForecast-day">{day()}</div>
-        <WeatherIconSmall code={props.data.condition.icon}/>
+        <WeatherIconSmall code={props.data.condition[0].icon}/>
         <div className="WeatherForescast-temp">
             <span className="WeatherForecast-temp-max">{maxTemperature()}</span>
             <span className="WeatherForecast-temp-min">{minTemperature()}</span>
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -11,7 +11,7 @@ export default function WeatherIcon(props){
         "03n": "cloudy",
         "04d": "cloudy",
         "04n": "cloudy",
-        "09d": "rainy",
+        "09d": "rainy-day",
         "09n": "rainy",
         "10d": "pouring",
         "10n": "pouring",
@@ -25,33 +25,7 @@ export default function WeatherIcon(props){
     }
 
     return <WeatherSvg 
-    state={codeMapping[props.code]} height={70} width={70}/>
-}import React from "react";
-import { WeatherSvg } from "weather-icons-animated";
-
-export default function WeatherIcon(props){
-    const codeMapping = {
-        "01d": "sunny",
-        "01n": "clear-night",
-        "02d": "partlycloudy",
-        "02n": "partlycloudy",
-        "03d": "cloudy",
-        "03n": "cloudy",
-        "04d": "cloudy",
-        "04n": "cloudy",
-        "09d": "rainy",
-        "09n": "rainy",
-        "10d": "pouring",
-        "10n": "pouring",
-        "11d": "lightning-rainy",
-        "11n": "lightning-rainy",
-        "13d": "snowy",
-        "13n": "snowy",
-        "50d": "fog",
-        "50n": "fog",
-
-    }
-
-    return <WeatherSvg 
-    icon={codeMapping[props.code]} height={70} width={70}/>
+    state={codeMapping[props.code]} 
+    width={90}
+    height={90}/>
 }
