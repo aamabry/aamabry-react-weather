efbac0ae3325d036aa7a52354b66b31564376c64
flangoria <audrey.mabry90@gmail.com>
2024-07-21 16:06:40 -0400

    Edited CSS

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -27,7 +27,7 @@ export default function WeatherInfo(props){
         </div>
         </div>
     </div>
-    <div className="col-6">
+    <div className="col-6 weather-description">
         <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h%</li>
